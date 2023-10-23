73112fc57b3c3635a1a2c469e018a24dbc338b84
JFMajer <jakubmajer@live.com>
2023-10-23 21:00:59 +0200

    added two new routes

--- main.go
+++ main.go
@@ -6,12 +6,21 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World!"))
+	w.Write([]byte("Hello from snippetbox!"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
 
 	log.Print("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
