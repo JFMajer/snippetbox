0d4c24b5e78f227fddbaa610ccd0e9a7eb9fa1d3
JFMajer <jakubmajer@live.com>
2023-11-19 12:00:35 +0100

    comments

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -8,16 +8,21 @@ import (
 	"strconv"
 )
 
+// home is the handler function for the root URL ("/").
+// It serves the home page of the web application.
 func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
+
+	// Define a slice containing the file paths for the base layout and home page templates
 	files := []string{
 		"./ui/html/pages/base.tmpl",
 		"./ui/html/pages/home.tmpl",
 	}
 
+	// Use the template.ParseFiles function to read the template files and store the templates in 'ts'
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Print(err.Error())
@@ -25,6 +30,8 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Execute the template, writing the generated HTML to the http.ResponseWriter.
+	// The "base" template is used as the 'layout' template.
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
