80d5d6dd0364ce0f171eab3c2cf20c220d8aac98
JFMajer <jakubmajer@live.com>
2023-11-19 20:16:29 +0100

    added docker compose and dockerfile

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -22,22 +22,13 @@ func main() {
 		infoLog: infoLog,
 	}
 
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
 	addr := flag.String("addr", ":4000", "HTTP Network Address")
 	flag.Parse()
 
 	srv := &http.Server{
 		Addr: *addr,
 		ErrorLog: errorLog,
-		Handler: mux,
+		Handler: app.routes(),
 	}
 	
 
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module snippetbox.heheszlo.com
 
-go 1.19
+go 1.21
