a0a96c1c474725d226db66badd81f8f272455319
JFMajer <jakubmajer@live.com>
2023-11-19 18:07:56 +0100

    better logging and much better errors

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,16 +3,15 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
 // home is the handler function for the root URL ("/").
 // It serves the home page of the web application.
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
@@ -26,8 +25,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	// Use the template.ParseFiles function to read the template files and store the templates in 'ts'
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, err)
 		return
 	}
 
@@ -35,17 +33,16 @@ func home(w http.ResponseWriter, r *http.Request) {
 	// The "base" template is used as the 'layout' template.
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w, err)
 	}
 }
 
 // snippetView is the handler function for the "/snippet/view" URL.
 // It displays a specific snippet based on an ID provided in the query string.
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 	fmt.Fprintf(w, "Display a specific snippet with an id %d", id)
@@ -53,11 +50,11 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 // snippetCreate is the handler function for the "/snippet/create" URL.
 // It handles the creation of new snippets.
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	// Check if the request method is POST, otherwise return a "Method Not Allowed" error.
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 	// If the request method is POST, create a new snippet.
